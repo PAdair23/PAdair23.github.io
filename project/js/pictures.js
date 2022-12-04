181de5b68eb1e9ac0fe8247ae9cbd9933f8cb235
PAdair23 <102702108+PAdair23@users.noreply.github.com>
2022-12-03 21:09:48 -0700

    Checked off HTML requirement list

---
+++ project/js/pictures.js
@@ -0,0 +1,19 @@
+function image(){
+document.getElementById("mainpic").src = "pictures/standard.jpg";
+document.getElementById("caption").innerHTML = "Nothing special about these. Just your typcial black keycaps.";
+}
+
+function image1(){
+document.getElementById("mainpic").src = "pictures/pudding.jpg";
+document.getElementById("caption").innerHTML = "Pudding keycaps offer opaque sides so that the lighting of your keyboard can really shine through.";
+}
+
+function image2(){
+document.getElementById("mainpic").src = "pictures/customp.jpg";
+document.getElementById("caption").innerHTML = "There are limitless designs for custom keycaps, this set has custom purple color, graphics, and shape.";
+}
+
+function image3(){
+document.getElementById("mainpic").src = "pictures/blanks.jpg";
+document.getElementById("caption").innerHTML = "There are limitless designs for custom keycaps, this set has no markings on them and is just plain white.";
+}
