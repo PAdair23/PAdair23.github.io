b75b37b9fadc20e2741ea833db077e008721ce2e
PAdair23 <102702108+PAdair23@users.noreply.github.com>
2022-12-12 21:55:50 -0700

    LOTSA THINGS
    Buttons working on keycap page, redo design of everything, mostly responsive now, working to get it verified with google.

--- project/js/pictures.js
+++ project/js/pictures.js
@@ -1,11 +1,23 @@
+console.log("testing for pull")
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    document.getElementById("monochrome").addEventListener("click",image);
+    document.getElementById("pudding").addEventListener("click",image1);
+    document.getElementById("purple").addEventListener("click",image2);
+    document.getElementById("blanks").addEventListener("click",image3);
+}
+
+
 function image(){
-document.getElementById("mainpic").src = "pictures/standard.jpg";
-document.getElementById("caption").innerHTML = "Nothing special about these. Just your typcial black keycaps.";
+    console.log("image called")
+    document.getElementById("mainpic").src = "pictures/monochrome.jpg";
+    document.getElementById("caption").innerHTML = "Black and white keycaps look great with most desk setups.";
 }
 
 function image1(){
-document.getElementById("mainpic").src = "pictures/pudding.jpg";
-document.getElementById("caption").innerHTML = "Pudding keycaps offer opaque sides so that the lighting of your keyboard can really shine through.";
+    document.getElementById("mainpic").src = "pictures/pudding.jpg";
+    document.getElementById("caption").innerHTML = "Pudding keycaps offer opaque sides so that the lighting of your keyboard can really shine through.";
 }
 
 function image2(){
